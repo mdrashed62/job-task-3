f16d4ea621aa1a4be4bbe4a848e1e18abee9aad8
rashed62 <mdrashedbiswas83@gmail.com>
2024-09-05 02:41:14 +0600

    Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import PartTwo from "../Components/PartTwo";
 
 const Home = () => {
   return (
-    <div  className='grid grid-cols-2 gap-4 pb-10'>
+    <div  className='grid grid-cols-2 gap-4 p-2'>
       {/* section one*/}
       <div className="bg-[#565758] md:w-full min-h-screen rounded-2xl">
 
@@ -14,7 +14,7 @@ const Home = () => {
     {/* section two */}
       <div>
       <PartOne/>
-      <div className="w-[500px] mt-4 mb-4 mx-auto h-[4px] rounded-2xl bg-[#313337]"></div>
+      <div className="w-[498px] mt-4 mb-4 mx-auto h-[4px] rounded-2xl bg-[#313337]"></div>
       <PartTwo/>
       </div>
       {/* end section two */}
